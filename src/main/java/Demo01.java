d77add6ffdb0f6bc5f919e59c7aaa975de18f8a1
root <root@itcase.cn>
2019-06-17 19:20:05 +0800

    Demo01中添加了内容

--- src/main/java/Demo01.java
+++ src/main/java/Demo01.java
@@ -2,5 +2,6 @@ public class Demo01 {
     public static void main(String[] args) {
         System.out.println("反反复复");
         System.out.println("噼里啪啦");
+        System.out.println("PNPNPNP");
     }
 }
